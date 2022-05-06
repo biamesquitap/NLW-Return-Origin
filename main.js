2a69032f994496d9e35dc38a89c33cfedc6b8eaa
Gabriel Patricio <gabriel.patricio@pbhub.com.br>
2022-05-06 00:04:16 -0300

    fix: Display flex para os icones ficarem um do lado do outro.

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function onScroll() {
   if (scrollY > 0) {
-    navigation.classList.add('scroll')
+    navigation.classList.add("scroll");
   } else {
-    navigation.classList.remove('scroll')
+    navigation.classList.remove("scroll");
   }
 }
 
 function openMenu() {
-  document.body.class.add('menu-expanded')
+  document.body.class.add("menu-expanded");
 }
 
 function closeMenu() {
-  document.body.class.remove('menu-expanded')
+  document.body.class.remove("menu-expanded");
 }
