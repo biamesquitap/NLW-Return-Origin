c0f5e5e08d03f6cf53fcf739c50d2653006bdf89
biamesquitap <beatrizmesquitta@yahoo.com>
2022-05-09 22:55:30 -0300

    36:35

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 function onScroll() {
   if (scrollY > 0) {
-    navigation.classList.add("scroll");
+    navigation.classList.add('scroll')
   } else {
-    navigation.classList.remove("scroll");
+    navigation.classList.remove('scroll')
   }
 }
 
 function openMenu() {
-  document.body.class.add("menu-expanded");
+  document.body.classList.add('menu-expanded')
 }
 
 function closeMenu() {
-  document.body.class.remove("menu-expanded");
+  document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
